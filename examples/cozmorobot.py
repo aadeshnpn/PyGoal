6489b57b936d4a4a168e63e97276899e213d91b5
aadeshnpn <neupaneaadesh@gmail.com>
2020-02-28 16:30:15 -0500

    cozmo trace working and experiment started

--- examples/cozmorobot.py
+++ examples/cozmorobot.py
@@ -7,19 +7,26 @@ GoalFrame work.
 import asyncio
 import cozmo
 from cozmo.util import degrees, distance_mm
+import copy
 
 from py_trees.trees import BehaviourTree
 from py_trees import Blackboard
-# from pygoal.lib.genrecprop import GenRecPropTaxi
+from pygoal.lib.genrecprop import GenRecProp
 from pygoal.utils.bt import goalspec2BT
+
+from flloat.parser.ltlfg import LTLfGParser
+
 
 from robot import ComplexGoal
 
 
-class CozmoPlanner:
+class CozmoPlanner(GenRecProp):
     def __init__(
             self, env, keys, goalspec, max_trace=40,
             actions=[0, 1, 2, 3], epoch=10, seed=None, policy=None):
+        super().__init__(
+            env, keys, goalspec, dict(), max_trace, actions, epoch, seed)
+        self.tcount = 0
         self.env = env
         self.keys = keys
         self.goalspec = goalspec
@@ -31,20 +38,97 @@ class CozmoPlanner:
         self.blackboard = Blackboard()
         self.blackboard.shared_content = dict()
 
+    def env_action_dict(self, action):
+        return action
+
+    def get_curr_state(self, env):
+        temp = list(env.decode(env.s))
+        return (str(temp[0])+str(temp[1]), temp[2], temp[3])
+
+    def set_state(self, env, trace, i):
+        state = []
+        for k in self.keys:
+            if k == 'L':
+                temp = trace[k][i][-1]
+                state.append(int(temp[0]))
+                state.append(int(temp[1]))
+            else:
+                temp = trace[k][i][-1]
+                state.append(int(temp))
+        state = env.encode(*tuple(state))
+        env.env.s = state
+
+    def create_trace_skeleton(self, state):
+        # Create a skeleton for trace
+        trace = dict(zip(self.keys, [[list()] for i in range(len(self.keys))]))
+        j = 0
+        for k in self.keys:
+            trace[k][0].append(state[j])
+            j += 1
+        trace['A'] = []
+        # Hack to fix the bug of terminal state. Adding big action number
+        # This makes the trace length same accross the keys
+        trace['A'].append(9)
+        return trace
+
+    def get_action_policy(self, policy, state):
+        action = policy[tuple(state)]
+        return action
+
+    def gtable_key(self, state):
+        ss = state
+        return tuple(ss)
+
+    def get_policy(self):
+        policy = dict()
+        for s, v in self.gtable.items():
+            elem = sorted(v.items(),  key=lambda x: x[1], reverse=True)
+            try:
+                policy[s] = elem[0][0]
+                pass
+            except IndexError:
+                pass
+
+        return policy
+
+    def evaluate_trace_simple(self, goalspec, trace):
+        # Test the prop algorithm
+        parser = LTLfGParser()
+        parsed_formula = parser(goalspec)
+        # Quick fix. create_trace_float requires action to be list of list
+        # temp = trace['A']
+        # trace['A'] = [temp]
+        # Create a trace compatiable with Flloat library
+        trace = self.list_to_trace(trace)
+        print(trace['CC'])
+        t = self.create_trace_flloat(trace, 0)
+        result = parsed_formula.truth(t)
+        return result
+
     def train(self, epoch=10, verbose=False):
         pass
 
-    def inference(self, render=False, verbose=False):
+    def inference(self, evaluate=True, verbose=False):
         # conn = cozmo.run.connect()
         print('inference', self.policy)
         # cozmo.run_program(
         #    self.policy, use_viewer=False, force_viewer_on_top=False)
         self.env(normal=False, goal=self.policy, tkeys=self.keys)
-        if self.blackboard.shared_content['status']:
-            print(self.blackboard.shared_content['states'])
+        # print(self.blackboard.shared_content['states'])
+        # self.
+        trace = self.blackboard.shared_content['states']
+        trace = copy.copy(trace)
+        if self.evaluate_trace_simple(self.goalspec, trace):
             return True
         else:
             return False
+
+        # if self.blackboard.shared_content['status']:
+        #     return True
+        # else:
+        #     return False
+
+
         # cozmo.run.connect(self.policy)
 
 # Task description for cozmo robot
@@ -66,11 +150,19 @@ def reset_env(robot):
 
 
 def cozmomain():
-    goal = 'F(P_[P][2,none,==])'
-    goalspec = '((((('+goal+' U '+goal+') U '+goal+') U '+goal+') U '+goal+') U '+goal+')'  # noqa:
+    goal1 = 'F(P_[DC][True,none,==])'
+    goal2 = 'F(P_[FC][True,none,==])'
+    goal3 = 'F(P_[CC][True,none,==])'
+    goal4 = 'G(P_[CC][True,none,==]) & F(P_[DD][True,none,==])'
+    goal5 = 'F(P_[FD][True,none,==])'
+    goal6 = 'F(P_[CC][False,none,==])'
+    # goal1 = 'F(P_[P][2,none,==])'
+    # goal1 = 'F(P_[P][2,none,==])'
+
+    goalspec = '((((('+goal1+' U '+goal2+') U '+goal3+') U '+goal4+') U '+goal5+') U '+goal6+')'  # noqa:
     # goalspec = goal+' U '+goal
     print(goalspec)
-    keys = ['P', 'DC', 'FC', 'CC', 'DD', 'FD', 'D']
+    keys = ['P', 'DC', 'FC', 'CC', 'DD', 'FD', 'D', 'A']
     # Pose, Detected Cube, Found Cube, Carried Cube, Detected Desk, Found Desk, Drop Desk
     # actions = [0, 1, 2, 3, 5]
     root = goalspec2BT(goalspec, planner=None)
@@ -83,13 +175,13 @@ def cozmomain():
     j = 0
     for child in behaviour_tree.root.children:
         # planner = planners[j]
-        planner = CozmoPlanner(crobot, keys, child.name, policy=policies[j])
+        planner = CozmoPlanner(ComplexGoal, keys, child.name, policy=policies[j])
         j += 1
         child.setup(0, planner, False, 5)
 
     for i in range(1):
         behaviour_tree.tick(
-            pre_tick_handler=reset_env(crobot)
+            pre_tick_handler=reset_env(ComplexGoal)
         )
         print(i, behaviour_tree.root.status)
 
@@ -103,7 +195,7 @@ def cozmomain():
 
 
 def cozmomain1():
-    goalspec = 'F(P_[P][2,none,==])'
+    goalspec = 'F(P_[DC][2,none,==])'
     # goalspec = '((((('+goal+' U '+goal+') U '+goal+') U '+goal+') U '+goal+') U '+goal+')'
     # goalspec = goal+' U '+goal
     print(goalspec)
@@ -131,11 +223,12 @@ def cozmomain1():
 
 
 def cozmomain2():
-    goal = 'F(P_[P][2,none,==])'
+    goal1 = 'F(P_[DC][True,none,==])'
+    goal2 = 'F(P_[FC][True,none,==])'
     # goalspec = '((((('+goal+' U '+goal+') U '+goal+') U '+goal+') U '+goal+') U '+goal+')'
-    goalspec = goal+' U '+goal
+    goalspec = goal1+' U '+goal2
     print(goalspec)
-    keys = ['P', 'DC', 'FC', 'CC', 'DD', 'FD', 'D']
+    keys = ['P', 'DC', 'FC', 'CC', 'DD', 'FD', 'D', 'A']
 
     # actions = [0, 1, 2, 3, 5]
     root = goalspec2BT(goalspec, planner=None)
@@ -159,7 +252,7 @@ def cozmomain2():
 
 
 def main():
-    cozmomain2()
+    cozmomain()
     # robot = ComplexGoal(normal=True, goal='detect_cube')
     # robot = ComplexGoal(normal=False, goal='detect_cube')
 
--- examples/robot.py
+++ examples/robot.py
@@ -5,6 +5,8 @@ import time
 import cozmo
 from cozmo.util import degrees, Pose, distance_mm, speed_mmps
 
+from cozmo.robot import MIN_LIFT_HEIGHT_MM
+
 from py_trees.trees import BehaviourTree
 from py_trees import Blackboard
 from scipy.spatial.distance import cdist
@@ -26,11 +28,17 @@ class ComplexGoal:
         self.goal = kw['goal']
         self.tkeys = kw['tkeys']
         normal = kw['normal']
+
+        self.states = dict(zip(self.tkeys, [list() for i in range(len(self.tkeys))]))
+
         if normal:
             cozmo.connect(self.runall)
         else:
             cozmo.connect(self.run)
         # self.crun = cozmo.connect()
+
+    def get_states(self):
+        return self.states
 
     def run(self, sdk_conn):
         '''The run method runs once Cozmo is connected.'''
@@ -58,23 +66,63 @@ class ComplexGoal:
         # self.states.append(self.robot.pose)
         # print(self.robot.pose)
         self.states['P'].append(self.robot.pose.position)
+        self.states['A'].append(9)
         dist = 1000
+        ddist = 1000
+        # print(dir(self.robot))
         if 'cube' in self.blackboard.shared_content.keys():
             self.states['DC'].append(True)
-            self.robot.pose.position
-            cube = self.blackboard.shared_content['cube']
-            dist = cdist(self.robot.pose.position, cube.pose.position, 'euclidean')
+            robot_pos = self.robot.pose.position
+            cube_pos = self.blackboard.shared_content['cube'].pose.position
+            # print(cube_pos, self.robot.lift_height.distance_mm, MIN_LIFT_HEIGHT_MM)
+            dist = cdist(
+                [[robot_pos.x, robot_pos.y, robot_pos.z]],
+                [[cube_pos.x, cube_pos.y, cube_pos.z]], 'euclidean')[0][0]
+
         else:
             self.states['DC'].append(False)
-        if dist < 67.0:
-            self.states['DC'].append(True)
+
+        if dist < 81.0:
+            self.states['FC'].append(True)
         else:
-            self.states['DC'].append(False)
+            self.states['FC'].append(False)
+
+        # if self.robot.is_carrying_block:
+        #     self.states['CC'].append(True)
+        # else:
+        #     self.states['CC'].append(False)
+        # self.robot.lift_height.distance_mm > MIN_LIFT_HEIGHT_MM
+        if (
+            self.robot.is_carrying_block is True
+                and self.robot.lift_height.distance_mm > MIN_LIFT_HEIGHT_MM
+                and self.robot.carrying_object_id != -1):
+            self.states['CC'].append(True)
+        else:
+            self.states['CC'].append(False)
+
+
+        if 'charger' in self.blackboard.shared_content.keys():
+            self.states['DD'].append(True)
+            robot_pos = self.robot.pose.position
+            dock_pos = self.blackboard.shared_content['charger'].pose.position
+            ddist = cdist(
+                [[robot_pos.x, robot_pos.y, robot_pos.z]],
+                [[dock_pos.x, dock_pos.y, dock_pos.z]], 'euclidean')[0][0]
+
+        else:
+            self.states['DD'].append(False)
+
+        if ddist < 81.0:
+            self.states['FD'].append(True)
+        else:
+            self.states['FD'].append(False)
 
 
     def detect_cube(self):
-        self.states = dict(zip(self.tkeys, [[] for i in range(len(self.tkeys))]))
+        # self.states = dict(zip(self.tkeys, [[] for i in range(len(self.tkeys))]))
         self.robot.world.add_event_handler(cozmo.robot.EvtRobotStateUpdated, self.collect_state)
+        self.robot.world.add_event_handler(cozmo.behavior.EvtBehaviorStopped, self.collect_state)
+        self.blackboard.shared_content['states'] = self.states
         look_around = self.robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
         if 'cube' in self.blackboard.shared_content.keys():
             cube = self.blackboard.shared_content['cube']
@@ -86,6 +134,7 @@ class ComplexGoal:
             print("Found cube", cube)
             self.blackboard.shared_content['cube'] = cube
             self.blackboard.shared_content['status'] = True
+
         except asyncio.TimeoutError:
             print("Didn't find a cube :-(")
             self.blackboard.shared_content['status'] = False
@@ -93,14 +142,16 @@ class ComplexGoal:
             # whether we find it or not, we want to stop the behavior
             look_around.stop()
             # print(self.states)
-            self.robot.world.remove_event_handler(cozmo.robot.EvtRobotStateUpdated, self.collect_state)
-            print('from detect cube',self.states)
-            self.blackboard.shared_content['states'] = self.states
+            # self.robot.world.remove_event_handler(cozmo.robot.EvtRobotStateUpdated, self.collect_state)
+            # print('from detect cube',self.states)
+
 
     def find_cube(self):
+        self.robot.world.add_event_handler(cozmo.robot.EvtRobotStateUpdated, self.collect_state)
+        self.robot.world.add_event_handler(cozmo.action.EvtActionCompleted, self.collect_state)
+        self.blackboard.shared_content['states'] = self.states
         cube = self.blackboard.shared_content['cube']
         # print('find cube', cube)
-
         action = self.robot.go_to_object(cube, distance_mm(75.0))
         # print(dir(action))
         action.wait_for_completed()
@@ -118,8 +169,14 @@ class ComplexGoal:
 
     def carry_cube(self):
         # Carry cube
+        self.robot.world.add_event_handler(cozmo.robot.EvtRobotStateUpdated, self.collect_state)
+        self.robot.world.add_event_handler(cozmo.action.EvtActionCompleted, self.collect_state)
+        self.blackboard.shared_content['states'] = self.states
+        # self.robot.world.add_event_handler(cozmo.)
+        # self.robot.world.add_event_handler(cozmo.behavior.EvtBehaviorStopped, self.collect_state)
         cube = self.blackboard.shared_content['cube']
         action = self.robot.pickup_object(cube)
+        # action = self.robot.dock_with_cube(cube)
         # print("got action", action)
         result = action.wait_for_completed(timeout=30)
         # print("got action result", result)
@@ -132,19 +189,22 @@ class ComplexGoal:
 
     def find_charger(self):
         # see if Cozmo already knows where the charger is
+        self.robot.world.add_event_handler(cozmo.robot.EvtRobotStateUpdated, self.collect_state)
+        self.robot.world.add_event_handler(cozmo.behavior.EvtBehaviorStopped, self.collect_state)
+        self.blackboard.shared_content['states'] = self.states
         charger = None
-        if self.robot.world.charger:
-            if self.robot.world.charger.pose.is_comparable(self.robot.pose):
-                print("Cozmo already knows where the charger is!")
-                charger = self.robot.world.charger
-                self.blackboard.shared_content['charger'] = charger
-                self.blackboard.shared_content['status'] = True
-            else:
-                # Cozmo knows about the charger, but the pose is not based on the
-                # same origin as the robot (e.g. the robot was moved since seeing
-                # the charger) so try to look for the charger first
-                self.blackboard.shared_content['status'] = False
-                # pass
+        # if self.robot.world.charger:
+        #     if self.robot.world.charger.pose.is_comparable(self.robot.pose):
+        #         print("Cozmo already knows where the charger is!")
+        #         charger = self.robot.world.charger
+        #         self.blackboard.shared_content['charger'] = charger
+        #         self.blackboard.shared_content['status'] = True
+        #     else:
+        #         # Cozmo knows about the charger, but the pose is not based on the
+        #         # same origin as the robot (e.g. the robot was moved since seeing
+        #         # the charger) so try to look for the charger first
+        #         self.blackboard.shared_content['status'] = False
+        #         # pass
 
         if not charger:
             # Tell Cozmo to look around for the charger
@@ -163,6 +223,8 @@ class ComplexGoal:
 
 
     def move_to_charger(self):
+        self.robot.world.add_event_handler(cozmo.robot.EvtRobotStateUpdated, self.collect_state)
+        self.blackboard.shared_content['states'] = self.states
         charger = self.blackboard.shared_content['charger']
         action = self.robot.go_to_object(charger, distance_mm(65.0))
         action.wait_for_completed()
@@ -175,12 +237,14 @@ class ComplexGoal:
 
 
     def drop_cube(self):
+        self.blackboard.shared_content['states'] = self.states
+        self.robot.world.add_event_handler(cozmo.robot.EvtRobotStateUpdated, self.collect_state)
         cube = self.blackboard.shared_content['cube']
         action = self.robot.place_object_on_ground_here(cube)
         print("got action", action)
-        result = action.wait_for_completed(timeout=30)
-        if not result.has_failed:
-            print("got action result", result)
+        action.wait_for_completed(timeout=30)
+        if not action.has_failed:
+            # print("got action result", result)
             self.robot.turn_in_place(degrees(90)).wait_for_completed()
             self.blackboard.shared_content['status'] = True
         else:
