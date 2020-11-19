b4723a3e487aa68c37892cb1f75d42fac676a80a
aadeshnpn <neupaneaadesh@gmail.com>
2020-11-18 19:46:47 -0500

    plotting working for aggregated data

--- examples/competent/experiments.py
+++ examples/competent/experiments.py
@@ -2,55 +2,8 @@
 
 from examples.competent.multigoalgrid import (
     MultiGoalGridExp)
+from pygoal.utils.distribution import plot_competency_variance
 import numpy as np
-import os
-import matplotlib
-# If there is $DISPLAY, display the plot
-if os.name == "posix" and "DISPLAY" not in os.environ:
-    matplotlib.use("Agg")
-
-import matplotlib.pyplot as plt  # noqa: E402
-
-# plt.style.use("fivethirtyeight")
-
-
-def draw_plots(data, name):
-    mean = np.mean(data, axis=1)
-    # Using quartile instead of mean and std
-    median = np.quantile(data, 0.5, axis=1)
-    q1 = np.quantile(data, 0.25, axis=1)
-    q3 = np.quantile(data, 0.75, axis=1)
-    fig = plt.figure()
-    # field_max = self.median + self.std
-    # field_min = self.mean - self.std
-    xvalues = range(1, len(median) + 1)
-    ax1 = fig.add_subplot(1, 1, 1)
-    # Plotting mean and standard deviation
-    ax1.plot(
-        xvalues,
-        median,
-        color="blue",
-        label="Median",
-        linestyle="dashdot",
-        linewidth=1.0,
-    )
-
-    ax1.plot(xvalues, mean, color="mediumblue", label="Mean", linewidth=1.0)
-
-    ax1.fill_between(xvalues, q1, q3, color="DodgerBlue", alpha=0.3)
-    plt.xlim(0, len(median))
-    ax1.legend()
-    ax1.set_xlabel("Time")
-    ax1.set_ylabel("Probability")
-
-    plt.tight_layout()
-    import pathlib
-    import os
-    dname = os.path.join('/tmp', 'pygoal', 'data', 'experiments')
-    pathlib.Path(dname).mkdir(parents=True, exist_ok=True)
-    fname = os.path.join(dname, name + '.png')
-    fig.savefig(fname)
-    plt.close(fig)
 
 
 def save_pickle(data, name):
@@ -68,10 +21,10 @@ def run_experiments(fn, name, runs=50):
     for i in range(runs):
         datas += [fn(i, name)]
 
-    print(datas, np.array(datas).shape)
+    # print(datas, np.array(datas).shape)
     # Plot the data
     save_pickle(datas, name)
-    draw_plots(datas, name)
+    plot_competency_variance(datas, name)
 
 
 def exp_find_key(expid, name='exp_find_key'):
--- pygoal/utils/distribution.py
+++ pygoal/utils/distribution.py
@@ -58,6 +58,65 @@ def scalef(std):
     return (np.sqrt(3) / np.pi) * std
 
 
+def plot_competency_variance(data, name):
+    # mean = np.mean(data, axis=1)
+    # Using quartile instead of mean and std
+    median = np.quantile(data, 0.5, axis=0)
+    q1 = np.quantile(data, 0.25, axis=0)
+    q3 = np.quantile(data, 0.75, axis=0)
+    fig = plt.figure()
+    xvalues = range(1, len(median) + 1)
+    # print(median.shape, median)
+    # Plotting mean and standard deviation using curve fitting
+    fig = plt.figure()
+    from scipy.optimize import curve_fit
+    try:
+        poptmed, _ = curve_fit(
+            logistfunc, xvalues, median,
+            maxfev=800)
+        poptq1, _ = curve_fit(
+            logistfunc, xvalues, q1,
+            maxfev=800)
+        poptq3, _ = curve_fit(
+            logistfunc, xvalues, q3,
+            maxfev=800)
+    except RuntimeError:
+        poptmed = np.array([1.0, 1.0, 1.0])
+        poptq1 = np.array([1.0, 1.0, 1.0])
+        poptq3 = np.array([1.0, 1.0, 1.0])
+
+    plt.plot(
+        range(len(median)), [0.8]*len(median), '*', label="$\\theta$",
+        color='tomato', linewidth=1, alpha=0.9)
+
+    plt.plot(
+        xvalues,
+        logistfunc(xvalues, *poptmed),
+        color="blue",
+        label='Median=%5.2f, $\mu$=%5.2f, s=%5.2f' % tuple(
+            poptmed), linewidth=1.0, alpha=0.8,
+        linestyle="dashdot",
+    )
+
+    plt.fill_between(
+        xvalues,
+        logistfunc(xvalues, *poptq1),
+        logistfunc(xvalues, *poptq3),
+        color="DodgerBlue", alpha=0.3)
+    plt.legend()
+    plt.ylabel('Probability')
+    plt.xlabel('Time')
+    plt.tight_layout()
+
+    import pathlib
+    import os
+    dname = os.path.join('/tmp', 'pygoal', 'data', 'experiments')
+    pathlib.Path(dname).mkdir(parents=True, exist_ok=True)
+    fname = os.path.join(dname, name + '.png')
+    fig.savefig(fname)
+    plt.close(fig)
+
+
 def compare_curve(
         competency, datas, visdata=True, vispdf=False,
         viscdf=True, name='competency', root=False):
@@ -152,7 +211,6 @@ def parallel(nodes):
         L = node[0] * nodes[i][0]
         node = [L, mean, std]
     return node
-
 
 
 def recursive_setup(node, fn_e, fn_c):
